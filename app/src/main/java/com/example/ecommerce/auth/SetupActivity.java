87347a83596edb4f2ad950d4ad7e9ebde005ca4e
Nishant Badaya <nishant7822badaya@gmail.com>
2021-05-27 16:31:37 +0530

    RegisterActivity LoginActivity

--- app/src/main/java/com/example/ecommerce/auth/SetupActivity.java
+++ app/src/main/java/com/example/ecommerce/auth/SetupActivity.java
@@ -2,7 +2,10 @@ package com.example.ecommerce.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.ecommerce.R;
@@ -16,11 +19,28 @@ public class SetupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_setup);
 
         initialize();
+        setListener();
     }
 
     private void initialize() {
 
         registerBN=findViewById(R.id.activity_setup_register_btn);
         loginBN=findViewById(R.id.activity_setup_login_btn);
+    }
+
+    private void setListener() {
+
+        registerBN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SetupActivity.this,RegisterActivity.class));
+            }
+        });
+        loginBN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SetupActivity.this,LoginActivity.class));
+            }
+        });
     }
 }
