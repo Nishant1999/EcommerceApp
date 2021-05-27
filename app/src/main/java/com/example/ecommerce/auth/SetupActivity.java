d35042900137c20e0d85f1dbe4ae0fe697056ce5
Nishant Badaya <nishant7822badaya@gmail.com>
2021-05-27 15:59:41 +0530

    Id

--- app/src/main/java/com/example/ecommerce/auth/SetupActivity.java
+++ app/src/main/java/com/example/ecommerce/auth/SetupActivity.java
@@ -3,14 +3,24 @@ package com.example.ecommerce.auth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.ecommerce.R;
 
 public class SetupActivity extends AppCompatActivity {
 
+    private Button registerBN,loginBN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+
+        initialize();
+    }
+
+    private void initialize() {
+
+        registerBN=findViewById(R.id.activity_setup_register_btn);
+        loginBN=findViewById(R.id.activity_setup_login_btn);
     }
 }
